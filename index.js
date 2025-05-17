2e926cb9c7397b93e8d6a5e869a62b3c4e4deb9b
JamanRain <138153561+JamanRain@users.noreply.github.com>
2025-05-17 21:08:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/grievances', {
 
 app.use('/api/grievances', grievanceRoutes);
 
-const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
