846ad4d1626b6aba5d729163c8745a148f9db1f0
JamanRain <138153561+JamanRain@users.noreply.github.com>
2025-05-20 12:12:29 +0530

    Update Grievance.js

--- models/Grievance.js
+++ models/Grievance.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const grievanceSchema = new mongoose.Schema({
-  username: String, // either "Mimansa" or "Raman169"
+  username: String, // Who submitted the grievance
+  recipient: String, // New: Who it's meant for
   title: String,
   details: String,
   severity: Number,
