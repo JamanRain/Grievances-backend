460e734f26f6acab6ea4c80332082b3a2c835aee
JamanRain <138153561+JamanRain@users.noreply.github.com>
2025-05-20 11:02:34 +0530

    Update grievanceRoutes.js

--- routes/grievanceRoutes.js
+++ routes/grievanceRoutes.js
@@ -11,23 +11,3 @@ router.put('/:id/respond', grievanceController.respondToGrievance);
 router.delete('/:id', grievanceController.deleteGrievance);
 
 module.exports = router;
-
-
-Database.js
-
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
-
-
